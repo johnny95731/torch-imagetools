3589fe7354ca14b5c25570d529766784bc6b92b0
johnny95731 <johnny95731@yahoo.com>
2026-02-15 05:52:06 +0800

    refacto: update tests of wavelets

--- wavelets/test_wavelets.py
+++ wavelets/test_wavelets.py
@@ -1,23 +1,26 @@
 import unittest
 
-from imgtools.wavelets import wavelets, _pywt_wrapping
+import torch
 from tests.basic import (
     BasicTest,
     get_img,
+    get_n_img_args,
     iter_dtype_device,
     run_over_all_dtype_device,
 )
 
+from imgtools.wavelets import _pywt_wrapping, wavelets
+
 
 class DWT(BasicTest):
-    def test_dwt(self):
+    def test_dwt2(self):
         self.print_name()
 
         wavelet = _pywt_wrapping.Wavelet('db2')
         arg_case = iter_dtype_device((wavelet.dec_low, wavelet.dec_high))
         for scaling, wavelet in arg_case:
             cases = run_over_all_dtype_device(
-                wavelets.dwt,
+                wavelets.dwt2,
                 scaling=scaling,
                 wavelet=wavelet,
             )
@@ -25,7 +28,24 @@ class DWT(BasicTest):
                 for r in res:
                     self._basic_assertion(inp, r, check_shape=False)
 
-    def test_dwt_partial(self):
+    def test_idwt2(self):
+        self.print_name()
+
+        wavelet = _pywt_wrapping.Wavelet('db2')
+        arg_case = iter_dtype_device((wavelet.dec_low, wavelet.dec_high))
+        for scaling, wavelet in arg_case:
+            for dtype in (torch.float32, torch.float64):
+                for device in ('cpu', 'cuda'):
+                    for inp in get_n_img_args(dtype=dtype, device=device):
+                        dec = wavelets.dwt2(
+                            inp[0], scaling=scaling, wavelet=wavelet
+                        )
+                        res = wavelets.idwt2(
+                            dec, scaling=scaling, wavelet=wavelet
+                        )
+                        self._basic_assertion(inp, res, check_shape=False)
+
+    def test_dwt2_partial(self):
         self.print_name()
 
         wavelet = _pywt_wrapping.Wavelet('db2')
@@ -37,12 +57,13 @@ class DWT(BasicTest):
             for inp in img_cases:
                 inp = inp[0]
                 for scaling, wavelet in arg_case:
-                    std = wavelets.dwt(inp, scaling, wavelet)
+                    std = wavelets.dwt2(inp, scaling, wavelet)
+                    print(inp.shape, std.shape, '\n')
                     for i, c in enumerate(('LL', 'LH', 'HL', 'HH')):
-                        res = wavelets.dwt_partial(
+                        res = wavelets.dwt2_partial(
                             inp, scaling, wavelet, target=c
                         )
-                        self._basic_assertion((std[i],), res)
+                        self._basic_assertion((std[..., i, :, :],), res)
 
 
 if __name__ == '__main__':
